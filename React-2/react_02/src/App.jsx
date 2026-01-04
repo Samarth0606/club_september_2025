fd62abf862b145e91c5ab62f1b1be1d8485abe92
Samarth Vohra <officialdevelopersamarth@gmail.com>
2026-01-04 21:20:35 +0530

    react lecture 2 code

---
+++ React-2/react_02/src/App.jsx
@@ -0,0 +1,29 @@
+// component - functonal
+
+function App(){
+  return(
+    <div>
+      <h1>Hello from App</h1>
+    </div>
+  )
+}
+
+export default App;
+
+
+// --------------------------------------
+
+// function 
+// function App(){
+//   return "samarth vohra" 
+// }
+// -------------------
+// component (fn)
+// function App(){
+//   return(
+//     // JSX (JS + HTML)
+//     <div>
+//       <h1>Hello From App</h1>
+//     </div>
+//   )
+// }
