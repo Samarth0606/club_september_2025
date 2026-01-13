92ea238e82aba4191f6b9f500e09f6606e74bf45
Samarth Vohra <officialdevelopersamarth@gmail.com>
2026-01-13 18:54:38 +0530

    react lecture 5 code

--- React-5/react_05/src/components/Questions_batching.jsx
+++ React-5/react_05/src/components/Questions_batching.jsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 function Questions(){
     const [count,setCount] = useState(0); 
     function handleJump(){
+        setCount((count)=>count+4) // 4
+        setCount((count)=>count+5) // 9
         setCount(count+3) // 3
-        setCount((count)=>count+4) // 7
-        setCount((count)=>count+5) // 12
+        setCount((count)=>count+5) // 3+5 = 8
     }
     function handleDec(x, y){
         console.log(x, y);
