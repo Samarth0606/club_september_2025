7488c78b935ebd581500403d58ff7ce1912f57b0
Samarth Vohra <officialdevelopersamarth@gmail.com>
2026-01-31 20:48:41 +0530

    react lecture 11 code

--- React-11/swiggy_01/src/App.jsx
+++ React-11/swiggy_01/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
+import ApiCalling from './components/ApiCalling'
 
 function App() {
   return (
     <div>
+      {/* <ApiCalling /> */}
       <Header />
       <Body />
       <Footer />
--- React-11/swiggy_01/src/components/Body.jsx
+++ React-11/swiggy_01/src/components/Body.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard'
+import ApiCalling from './ApiCalling'
 
 function Body() {
+    let restArr = ApiCalling();
   return (
     <>
         <div>
@@ -10,15 +12,7 @@ function Body() {
             <button className='border rounded-xl w-1/12 p-1 ml-10 mt-3 text-xl'>Reset</button>
         </div>
         <div className='flex flex-wrap w-10/12 m-auto'>
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
+            <RestaurantCard restArr={restArr}/>
         </div>
     </>
   )
